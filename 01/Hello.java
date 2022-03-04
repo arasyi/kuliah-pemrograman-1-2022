b122092e41d19e0b732dccde8d8f2bbc2bac1b30
Mohammad Arif Rasyidi <arasyi@gmail.com>
2022-03-04 19:28:17 +0700

    Kuliah Minggu 1

---
+++ 01/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+	public static void main(String[] args) {
+		// generate some simple output
+		/*
+		System.out.println("Hello, World!\n"); // first line
+		System.out.print("Hello, World!\n"); // 2nd line
+		*/
+		System.out.print("Hello, World!"); // ini adalah komen
+		// ini adalah komen
+		
+		System.out.println();
+		
+		// comments
+		// djwkdjlkwdml fjewklfwekldf jkldwefnmdfklwemlkd
+	}
+}
