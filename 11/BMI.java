4793304fde194c669769b6fd1e84f2fcc393e0de
Mohammad Arif Rasyidi <arasyi@gmail.com>
2022-05-19 10:56:03 +0700

    Kuliah Minggu 11

--- 11/BMI.java
+++ 11/BMI.java
@@ -7,19 +7,19 @@ public class BMI {
 	 * @param height tinggi badan dalam meter (m)
 	 * @return hasil perhitungan BMI
 	 */
-    public static double computeBMI(double weight, double height) {
-        double bmi = weight / height / height;
-        
-        return bmi;
-    }
-    
-    public static void main(String[] args) {
-        double weightAni = 40;
-        double heightAni = 1.65;
-        
-        double bmiAni = computeBMI(weightAni, heightAni);
-        
-        System.out.println("Ani's BMI is " + bmiAni);
-        System.out.println(weightAni);
-    }
+	public static double computeBMI(double weight, double height) {
+		double bmi = weight / height / height;
+		
+		return bmi;
+	}
+	
+	public static void main(String[] args) {
+		double weightAni = 40;
+		double heightAni = 1.65;
+		
+		double bmiAni = computeBMI(weightAni, heightAni);
+		
+		System.out.println("Ani's BMI is " + bmiAni);
+		System.out.println(weightAni);
+	}
 }
